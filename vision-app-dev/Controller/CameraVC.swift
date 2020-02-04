2bbbd3f7157923edc67b5d10fdb5c231327abd59
piteli <rushwender@gmail.com>
2020-02-04 21:38:53 +0800

    updated

--- vision-app-dev/Controller/CameraVC.swift
+++ vision-app-dev/Controller/CameraVC.swift
@@ -10,6 +10,10 @@ import UIKit
 import AVFoundation
 import CoreML
 import Vision
+enum FlashState {
+    case off
+    case on
+}
 
 class CameraVC: UIViewController {
     
@@ -17,6 +21,8 @@ class CameraVC: UIViewController {
     var cameraOutput: AVCapturePhotoOutput!
     var previewLayer: AVCaptureVideoPreviewLayer!
     var photoData : Data?
+    
+    var flashControlState : FlashState = .off
 
     @IBOutlet weak var captureImageView: RoundedImageView!
     @IBOutlet weak var flashBtn: RoundedShadowButton!
@@ -27,6 +33,7 @@ class CameraVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -73,6 +80,12 @@ class CameraVC: UIViewController {
         
         settings.previewPhotoFormat = settings.embeddedThumbnailPhotoFormat
         
+        if flashControlState == .off {
+            settings.flashMode = .off
+        } else {
+            settings.flashMode = .on
+        }
+        
         cameraOutput.capturePhoto(with: settings, delegate: self)
         
     }
@@ -93,6 +106,18 @@ class CameraVC: UIViewController {
             }
         }
     }
+    
+    @IBAction func flashBtnWasPressed(_ sender: Any) {
+        switch flashControlState {
+            case .off:
+                flashBtn.setTitle("FLASH ON", for : .normal)
+                flashControlState = .on
+            case .on:
+                flashBtn.setTitle("FLASH OFF", for: .normal)
+                flashControlState = .off
+        }
+    }
+    
 }
 
 extension CameraVC : AVCapturePhotoCaptureDelegate {
